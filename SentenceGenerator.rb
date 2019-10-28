fc3359efe7eeddca2d473a138d6e80c19fa3ce81
baldwinr <38447772+baldwinr@users.noreply.github.com>
2019-10-28 13:18:47 -0500

    Update and rename Prog2.rb to SentenceGenerator.rb

--- SentenceGenerator.rb
+++ SentenceGenerator.rb
@@ -1,4 +1,4 @@
-# This is the main class for program 2, which was provided by the instructor.
+# This is the main class for the context free sentence generator.
 # It instaniates a CFGrammar class and calls its run method. This
 # will prompt the user to enter context free grammar rules and print
 # all possible sentences.
